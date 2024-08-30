44b8ddcf4c03c720e12d38a45cc36d7cc73dc3e1
Jacob Smith <jacobskip7@gmail.com>
2024-08-30 18:13:21 -0400

    Added metadata to the contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -4,9 +4,46 @@ import HeroSection from '../components/Sections/HeroSection';
 import homeHero from '../../../public/images/home-hero.webp';
 import ContactForm from '../components/Forms/ContactForm';
 import FormSection from '../components/Sections/FormSection';
+import logo from '../../../public/images/logo-bottom.png';
 
 export const metadata = {
-  title: 'Contact Us | Quick Claim Supplements',
+  title:
+    'Contact Quick Claim Supplements | Expert Supplementing & Estimating Services',
+  description:
+    'Contact Quick Claim Supplements today to maximize your insurance claims. We offer expert supplementing and estimating services with personalized attention to every detail. Let us help you secure top dollar for your claims.',
+  keywords: [
+    'contact Quick Claim Supplements',
+    'insurance claim supplementing',
+    'claim estimating services',
+    'maximize insurance claims',
+    'personalized service',
+    'insurance claims help',
+  ],
+  openGraph: {
+    title:
+      'Contact Quick Claim Supplements | Expert Supplementing & Estimating Services',
+    description:
+      'Reach out to Quick Claim Supplements to ensure you get the most out of your insurance claims. We provide expert supplementing and estimating services with a focus on personalized attention and maximizing your returns.',
+    image: [
+      {
+        url: logo.src,
+        width: 500,
+        height: 500,
+        alt: 'Quick Claim Supplements Logo',
+      },
+      {
+        url: homeHero.src,
+        width: 1280,
+        height: 854,
+        alt: 'Professional working on documents',
+      },
+    ],
+    url: '#/contact',
+    type: 'website',
+  },
+  alternatives: {
+    canonical: '#/contact',
+  },
 };
 
 export default function Contact() {
